5634fd43ee24d5cd14dca997876211410e6e2b1f
nhatkent <nhat@vinadesign.vn>
2016-05-16 00:55:11 +0700

    launh first page
    amazing

---
+++ assets/danhgia/danhgia_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){"use strict";var _=null,y=true,n=false,x2='5.0.0.375',x3='rgba(0,0,0,0)',g='image',x6='rgba(234,158,83,1.00)',x1='5.0.0',i='none';var g5='phone_mock.png',g4='danhgia_screen.gif';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x1,b:x2,stf:i,cg:i,rI:n,cn:{dom:[{id:'danhgia_screen',t:g,r:['71px','163px','416px','644px','auto','auto'],f:[x3,im+g4,'0px','0px']},{id:'phone_mock',t:g,r:['36px','12px','478px','788px','auto','auto'],f:[x3,im+g5,'0px','0px']}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'550px','800px'],overflow:'hidden',f:[x6]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-1642420101");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-1642420101");
